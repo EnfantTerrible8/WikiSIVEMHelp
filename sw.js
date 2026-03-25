3b43cb4d236992763fb8660da3db3988fe235c7a
EnfantTerrible8 <diogo.goncalves.enf@gmail.com>
2026-03-25 16:56:13 +0000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+
+const CACHE="wikisiv-final";
+self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html"])))});
+self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
